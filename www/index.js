3f09f837826919d8042348d9681a718073aa55d7
alberto-gonzalez <albertito@hotmail.com>
2015-04-24 16:23:02 +0200

    html y js

---
+++ www/index.js
@@ -0,0 +1,37 @@
+var API_URL = "http://localhost:8080/mvprueba-api/myresource";
+
+
+/*
+Details about repository of GitHub API 
+https://developer.github.com/v3/repos/
+*/
+
+$("#button_got_it").click(function(e) {
+	e.preventDefault();
+	getMensaje();
+});
+
+
+
+
+function getMensaje() {
+	var url = API_URL;
+	$("#list_result").text('');
+
+	$.ajax({
+		url : url,
+		type : 'GET',
+		crossDomain : true,
+		dataType : 'json',
+	}).done(function(data, status, jqxhr) {
+
+				var gotit = data;
+				console.log(gotit)
+				$("#list_result").text(gotit);
+				
+				
+			}).fail(function() {
+				$('<div class="alert alert-danger"> <strong>Oh!</strong> Repository not found </div>').appendTo($("#result"));
+	});
+
+}
